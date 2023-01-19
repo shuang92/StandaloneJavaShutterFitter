5db911a48c62144cbcc0a9312d7351bc6f0f91ed
Tony Johnson <tonyj@stanford.edu>
2023-01-19 09:20:22 -0800

    add apache-commons-math as dependency add simple fitting example

--- src/main/java/org/lsst/ccs/standalone/shutter/fitter/StandaloneJavaShutterFitter.java
+++ src/main/java/org/lsst/ccs/standalone/shutter/fitter/StandaloneJavaShutterFitter.java
@@ -2,6 +2,9 @@ package org.lsst.ccs.standalone.shutter.fitter;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
+import org.apache.commons.math3.fitting.PolynomialCurveFitter;
+import org.apache.commons.math3.fitting.WeightedObservedPoints;
 import org.lsst.ccs.standalone.shutter.fitter.json.MotionDoneReaderWriter;
 import org.lsst.ccs.subsystem.shutter.common.EncoderSample;
 import org.lsst.ccs.subsystem.shutter.common.HallTransition;
@@ -17,22 +20,6 @@ public class StandaloneJavaShutterFitter {
         
         MotionDoneReaderWriter readerWriter = new MotionDoneReaderWriter();
 
-//        EncoderSample es = new EncoderSample(CCSTimeStamp.currentTime(), 50);
-//        HallTransition ht = new HallTransition(CCSTimeStamp.currentTime(), 6, 60, true);
-//        MotionDone md
-//                = new MotionDone.Builder()
-//                        .side(ShutterSide.MINUSX)
-//                        .startPosition(0)
-//                        .targetPosition(100)
-//                        .startTime(CCSTimeStamp.currentTime())
-//                        .targetDuration(Duration.ofMillis(900))
-//                        .endPosition(100)
-//                        .actualDuration(Duration.ofMillis(999))
-//                        .encoderSamples(Collections.singletonList(es))
-//                        .hallTransitions(Collections.singletonList(ht))
-//                        .build();
-//        objectMapper.writeValue(new File("target/car.json"), md);
-
         // TODO: Replace this with code which reads from .ser file
         URL json = StandaloneJavaShutterFitter.class.getResource("/shutterMotion.json");
         MotionDone md = readerWriter.read(json);
@@ -45,5 +32,20 @@ public class StandaloneJavaShutterFitter {
         for (HallTransition ht : md.hallTransitions()) {
             System.out.printf("EncoderSample: %d %g %d %s\n", ht.getTime().getUTCInstant().toEpochMilli() , ht.getPosition(), ht.getSensorId(), ht.isOn());
         }
+        
+        // Lets do a simple fit to the hall sensors
+        // See: https://commons.apache.org/proper/commons-math/userguide/fitting.html
+        // See: https://commons.apache.org/proper/commons-math/javadocs/api-3.6.1/overview-summary.html
+        
+        final WeightedObservedPoints obs = new WeightedObservedPoints();
+        for (HallTransition ht : md.hallTransitions()) {
+            obs.add(ht.getTime().getUTCInstant().toEpochMilli(), ht.getPosition());
+        }
+        
+        final PolynomialCurveFitter fitter = PolynomialCurveFitter.create(3);
+        final double[] coeff = fitter.fit(obs.toList());
+        System.out.println(Arrays.toString(coeff));
+        
+        // TODO: provide a better example using SimpleCurveFitter and ParametricUnivariateFunction
     }
 }
