ba5c370b29a7c47b51be0d9e015bf54600a44483
Shuang Liang <shuang@dhcp-visitor-enr-113-53.slac.stanford.edu>
2023-01-27 17:25:32 -0800

    working example

--- src/main/java/org/lsst/ccs/standalone/shutter/fitter/StandaloneJavaShutterFitter.java
+++ src/main/java/org/lsst/ccs/standalone/shutter/fitter/StandaloneJavaShutterFitter.java
@@ -1,5 +1,7 @@
 package org.lsst.ccs.standalone.shutter.fitter;
 
+import java.lang.reflect.*;
+import java.lang.Math;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -30,13 +32,19 @@ public class StandaloneJavaShutterFitter {
         MotionDone md = readerWriter.read(json);
         System.out.println(md);
 
-        for (EncoderSample es : md.encoderSamples()) {
-            System.out.printf("EncoderSample: %d %g\n", es.getTime().getUTCInstant().toEpochMilli(), es.getPosition());
-        }
+	long startTime = md.startTime().getUTCInstant().toEpochMilli();
+	double actualDuration = md.actualDuration().getSeconds() + md.actualDuration().getNano()*1e-9;
 
-        for (HallTransition ht : md.hallTransitions()) {
-            System.out.printf("HallTransition: %d %g %d %s\n", ht.getTime().getUTCInstant().toEpochMilli(), ht.getPosition(), ht.getSensorId(), ht.isOn());
-        }
+	double startPosition = md.startPosition();
+	double endPosition = md.endPosition();
+
+	String direction = "x";
+	if (startPosition < endPosition) {
+		direction = "+";
+	} else if (startPosition > endPosition) {
+		direction = "-";
+	}
+	System.out.println(direction);
 
         // Lets do a simple fit to the hall sensors
         // See: https://commons.apache.org/proper/commons-math/userguide/fitting.html
@@ -51,38 +59,81 @@ public class StandaloneJavaShutterFitter {
         System.out.println(Arrays.toString(coeff));
 
         // Try a better example using SimpleCurveFitter and ParametricUnivariateFunction
-        double t0 = 0.0;
-        double t1 = 0.3;
-        double t2 = 0.6;
-        double j0 = 30000;
-        double j1 = -30000;
-        double j2 = 30000;
-        double[] startPoint = {t0, t1, t2, j0, j1, j2};
+        double t0p = 0.00;
+        double t1p = 0.225;
+        double t2p = 0.675;
+        double j0p = 30000;
+        double j1p = -30000;
+        double j2p = 30000;
+        double[] startPoint_physical = {t0p, t1p, t2p, j0p, j1p, j2p};
+
+        double t0s = 0.00 / actualDuration;
+        double t1s = 0.225 / actualDuration;
+        double t2s = 0.675 / actualDuration;
+        double j0s = 30000 * Math.pow(actualDuration, 3) / 750;
+        double j1s = -30000 * Math.pow(actualDuration, 3) / 750;
+        double j2s = 30000 * Math.pow(actualDuration, 3) / 750;
+        double[] startPoint_scaled = {t0s, t1s, t2s, j0s, j1s, j2s};
 
         SixParameterModel model = new SixParameterModel();
-        long timeOffset = 1657846414500L;
 
-        // Convert times to seconds since timeOffset
-        double[] times = md.hallTransitions().stream().mapToDouble(ht -> (ht.getTime().getUTCInstant().toEpochMilli() - timeOffset) / 1000.0).toArray();
-        double[] positions = md.hallTransitions().stream().mapToDouble(ht -> ht.getPosition()).toArray();
+        // Convert times to seconds since startTime
+        double[] times_physical = md.hallTransitions().stream().mapToDouble(ht -> (ht.getTime().getUTCInstant().toEpochMilli() - startTime) / 1000.0).toArray();
+	double[] positions_physical = md.hallTransitions().stream().mapToDouble(ht -> ht.getPosition()).toArray();
+
+	// append the last data point from MotionDone meta data
+	times_physical = Arrays.copyOf(times_physical, times_physical.length+1);
+	positions_physical = Arrays.copyOf(positions_physical, positions_physical.length+1);
+
+	times_physical[times_physical.length-1] = actualDuration;
+	positions_physical[positions_physical.length-1] = endPosition;
+
+	// normalize the data to [0,1]
+	int dataNumber = times_physical.length;
+	double[] times_scaled = new double[dataNumber];
+	double[] positions_scaled = new double[dataNumber];
+
+        for (int i = 0; i < dataNumber; i++) {
+		times_scaled[i] = times_physical[i] / actualDuration;
+		if (direction.equals("+")) {
+			positions_scaled[i] = (positions_physical[i] - startPosition) / 750;
+		} else if (direction.equals("-")) {
+			positions_scaled[i] = (startPosition - positions_physical[i]) / 750;
+		}
+	}
+
         final List<WeightedObservedPoint> points = new ArrayList<>();
 
         System.out.println("====================================== Start point");
-        for (int i = 0; i < times.length; i++) {
-            double p = model.value(times[i], t0, t1, t2, j0, j1, j2);
-            System.out.printf("HallTransition: %g %g %g\n", times[i], positions[i], p);
-            points.add(new WeightedObservedPoint(1.0, times[i], positions[i]));
+        System.out.printf("HallTransition: %s %s %s %s %s %s\n", "scaled time", "positions", "model;", "physical time", "positions", "model");
+        for (int i = 0; i < dataNumber; i++) {
+            double p_scaled = model.value(times_scaled[i], t0s, t1s, t2s, j0s, j1s, j2s);
+            double p_physical = model.value(times_physical[i], t0p, t1p, t2p, j0p, j1p, j2p);
+            System.out.printf("HallTransition: %g %g %g; %g %g %g\n", times_scaled[i], positions_scaled[i], p_scaled, 
+									times_physical[i], positions_physical[i], p_physical);
+            points.add(new WeightedObservedPoint(1.0, times_scaled[i], positions_scaled[i]));
         }
-        System.out.printf("grad: %s\n", Arrays.toString(model.gradient(0.5, startPoint)));
+        System.out.printf("grad: %s\n", Arrays.toString(model.gradient(0.5, startPoint_scaled)));
 
-        SimpleCurveFitter curveFitter = SimpleCurveFitter.create(model, startPoint);
-        double[] fit = curveFitter.fit(points);
-        System.out.println("Fit result: "+Arrays.toString(fit));
+        SimpleCurveFitter curveFitter = SimpleCurveFitter.create(model, startPoint_scaled);
+        double[] fit_scaled = curveFitter.fit(points);
+	double[] fit_physical = new double[fit_scaled.length];
+	fit_physical[0] = fit_scaled[0] * actualDuration;
+	fit_physical[1] = fit_scaled[1] * actualDuration;
+	fit_physical[2] = fit_scaled[2] * actualDuration;
+	fit_physical[3] = fit_scaled[3] * 750 / Math.pow(actualDuration, 3);
+	fit_physical[4] = fit_scaled[4] * 750 / Math.pow(actualDuration, 3);
+	fit_physical[5] = fit_scaled[5] * 750 / Math.pow(actualDuration, 3);
+
+        System.out.println("Fit result: "+Arrays.toString(fit_physical));
         System.out.println("====================================== After fit");
-        for (int i = 0; i < times.length; i++) {
-            double p = model.value(times[i], fit);
-            System.out.printf("HallTransition: %g %g %g\n", times[i], positions[i], p);
-        }        
+        System.out.printf("HallTransition: %s %s %s %s %s %s\n", "scaled time", "positions", "model;", "physical time", "positions", "model");
+        for (int i = 0; i < times_physical.length; i++) {
+            double p_scaled = model.value(times_scaled[i], fit_scaled);
+            double p_physical = model.value(times_physical[i], fit_physical);
+            System.out.printf("HallTransition: %g %g %g; %g %g %g\n", times_scaled[i], positions_scaled[i], p_scaled, 
+									times_physical[i], positions_physical[i], p_physical);
+        }   
     }
 
     private static class SixParameterModel implements ParametricUnivariateFunction {
@@ -98,9 +149,12 @@ public class StandaloneJavaShutterFitter {
             double j1 = parameters[4];
             double j2 = parameters[5];
 
-            if (t < t0) {
-                return 0;
-            } else if (t < t1) {
+//            if (t < t0) {
+//                return 0;
+//           } else
+
+	    t = t - t0;
+	    if (t < t1) {
                 return j0 * t * t * t / 6;
             } else {
                 double A1 = (j0 - j1) * t1;
